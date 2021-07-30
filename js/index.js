325bce8c4367db1e6bdc00d243d5745639926017
renatoGalloAlves <renato_gallo_20@hotmail.com>
2021-07-30 12:13:50 -0300

    melhorias no js

--- js/index.js
+++ js/index.js
@@ -16,6 +16,27 @@
     celular: document.querySelector('[name="campo-celular"]')
   }
 
+  function mostraResultado(campos) {
+    const { 
+      nome,
+      cpf, 
+      rg, 
+      genero,
+      endereco, 
+      numero, 
+      cep, 
+      bairro, 
+      cidade, 
+      estado, 
+      telefone, 
+      celular } = campos;
+    let generoValue = Array.from(genero).find(r => r.checked).value;
+    const cadastro = JSON.stringify({cpf: cpf.value, rg: rg.value, genero: generoValue, endereco: endereco.value, numero: numero.value, cep: cep.value, bairro: bairro.value, cidade: cidade.value, estado: estado.value.toUpperCase(), telefone: telefone.value, celular: celular.value})
+    
+    alert(`Olá, ${nome.value}! Obrigado por se cadastrar!`)
+    localStorage.setItem('cadastro', cadastro)
+  }
+
   function mascaraCPF(cpf){
     cpf=cpf.replace(/\D/g,"")
     cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
@@ -28,18 +49,6 @@
     cep=cep.replace(/\D/g,"")
     cep=cep.replace(/(\d{5})(\d)/,"$1-$2")
     return cep
-  }
-
-  function mostraResultado(campos) {
-    const { nome,
-      cpf, 
-      rg, 
-      genero, endereco, numero, cep, bairro, cidade, estado, telefone, celular } = campos;
-    let generoValue = Array.from(genero).find(r => r.checked).value;
-    const cadastro = JSON.stringify({cpf: cpf.value, rg: rg.value, genero: generoValue, endereco: endereco.value, numero: numero.value, cep: cep.value, bairro: bairro.value, cidade: cidade.value, estado: estado.value.toUpperCase(), telefone: telefone.value, celular: celular.value})
-    
-    alert(`Olá, ${nome.value}! Obrigado por se cadastrar!`)
-    localStorage.setItem('cadastro', cadastro)
   }
 
   function mascaraTelefone(numeroTelefone) {
@@ -64,10 +73,6 @@
   
     if (celular.length > 11) {
       celular = celular.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-    } else if (r.length > 7) {
-      celular = celular.replace(/^(\d\d)(\d{5})(\d{0,4}).*/, "($1) $2-$3");
-    } else if (r.length > 2) {
-      celular = celular.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
     } else if (v.trim() !== "") {
       celular = celular.replace(/^(\d*)/, "($1");
     }
